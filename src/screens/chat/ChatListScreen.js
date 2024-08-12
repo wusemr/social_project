39905233f93baab2f1b5ea5199395693bdef1a4d
김지연 <>
2024-08-13 04:27:33 +0900

    Create Chat Screens (Empty View) ChatList, Chat

---
+++ src/screens/chat/ChatListScreen.js
@@ -0,0 +1,18 @@
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity
+} from "react-native"
+
+const ChatListScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>채팅 리스트 화면입니다.</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default ChatListScreen
---
+++ src/screens/chat/ChatScreen.js
@@ -0,0 +1,18 @@
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity
+} from "react-native"
+
+const ChatScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>실시간 채팅 화면입니다.</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default ChatScreen
