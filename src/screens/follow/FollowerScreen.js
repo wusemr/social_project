e3ae5a274b1984021972beb6c35fd4df9d13ed13
김지연 <>
2024-08-13 04:28:10 +0900

    Create Follow Screens (Empty View) Follower, Following

---
+++ src/screens/follow/FollowerScreen.js
@@ -0,0 +1,18 @@
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity
+} from "react-native"
+
+const FollowerScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>팔로워 화면입니다.</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default FollowerScreen
---
+++ src/screens/follow/FollowingScreen.js
@@ -0,0 +1,18 @@
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity
+} from "react-native"
+
+const FollowingScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>팔로잉 목록 화면입니다.</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default FollowingScreen
