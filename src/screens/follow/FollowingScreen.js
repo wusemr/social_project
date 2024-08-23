442e3a389d615b328a28ae7f5836e854fc9151bb
김지연 <>
2024-08-23 22:17:45 +0900

    팔로잉 목록 화면 수정

--- src/screens/follow/FollowingScreen.js
+++ src/screens/follow/FollowingScreen.js
@@ -1,15 +1,83 @@
+import { useEffect, useState } from "react"
 import {
     SafeAreaView,
     View,
     Text,
-    TouchableOpacity
+    FlatList
 } from "react-native"
+import { Server } from "@env"
+import FollowListItem from "../../components/FollowListItem"
+import { CONTAINER, TYPOGRAPHY } from "../../styles/commonStyles"
 
-const FollowingScreen = () => {
+const FollowingScreen = ({ route }) => {
+    const { currentUser, targetUser } = route.params
+    const [followingList, setFollowingList] = useState([])
+
+    const fetchFollowingList = async () => {
+        try {
+            const response = await fetch(`${Server}/user/get-list/following?currentUser=${currentUser}&targetUser=${targetUser}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            if (!response.ok) {
+                throw new Error('팔로잉 목록을 불러오는 중 오류가 발생했습니다.');
+            }
+
+            const data = await response.json();
+            setFollowingList(data);
+        } catch (error) {
+            console.error('팔로잉 목록을 불러오는 중 오류가 발생했습니다.', error);
+        }
+    }
+
+    const renderItem = ({ item }) => (
+        <FollowListItem
+            profile_picture={item.profile_picture}
+            username={item.username}
+            userid={item.userid}
+            isFollowing={item.isFollowing}
+            onFollowToggle={() => handleFollowButton(item.userid, item.isFollowing)}
+        />
+    )
+
+    const handleFollowButton = async (userid, isFollowing) => {
+        const target = userid
+
+        try {
+            const endpoint = isFollowing ? `${Server}/user/unfollow` : `${Server}/user/follow`;
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ userid: currentUser, following_id: target })
+            });
+
+            if (!response.ok) {
+                throw new Error('팔로우 상태를 변경하는 중 오류가 발생했습니다.');
+            }
+
+            fetchFollowingList();
+        } catch (error) {
+            console.error('팔로우 상태를 변경하는 중 오류가 발생했습니다.', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchFollowingList()
+    }, [targetUser])
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>팔로잉 목록 화면입니다.</Text>
+        <SafeAreaView style={CONTAINER.container}>
+            <View style={[CONTAINER.header, { justifyContent: 'center', alignItems: 'center', height: '5%', marginBottom: 5 }]} >
+                <Text style={TYPOGRAPHY.bigText}>팔로우</Text>
+            </View>
+
+            <View style={{flex: 1}}>
+                <FlatList
+                    data={followingList}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
             </View>
         </SafeAreaView>
     )
