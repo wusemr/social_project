a9ac4532ab227e4379dec2799b6f0a16a52aca4a
김지연 <>
2024-10-17 16:54:31 +0900

    각 게시물 형식 구현 중

---
+++ web/social_web/src/components/PostFormat.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const PostFormat = ({
+    profilePic,
+    userid,
+    postImages,
+    likeCount,
+    caption,
+    commentCount,
+    handleLike,
+    isLiked,
+    viewLikeList,
+    goToProfile,
+    viewComments
+}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PostFormat
