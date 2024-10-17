c35a484305ae77f4ff2bcc2712388a383d440f80
김지연 <>
2024-10-17 16:52:24 +0900

    로그인 프론트, 백 연동 (스타일 적용 X)

--- web/social_web/src/pages/Login.js
+++ web/social_web/src/pages/Login.js
@@ -3,29 +3,48 @@ import { useNavigate } from "react-router-dom"
 import { UserContext } from "../auth/UserContext"
 
 const Login = () => {
+    const server = process.env.REACT_APP_SERVER_URL
+
     const navigate = useNavigate()
 
     const { login } = useContext(UserContext)
 
     const [id, setId] = useState('')
     const [password, setPassword] = useState('')
+    const [loginCheckLabel, setLoginCheckLabel] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
 
     // 로그인 수행 함수
     const handleLogin = async (e) => {
         e.preventDefault()
 
-        const adminData = {
-            id: "qwer",
-            password: "qwer"
+        // 공란 여부 검사
+        if (!id || !password) {
+            setLoginCheckLabel('로그인 정보를 모두 입력해주세요.')
         }
 
-        if (id === adminData.id && password === adminData.password) {
-            login({ id })
-            navigate("/")
-            clearAll()
-        } else {
-            alert("아이디 또는 비밀번호가 잘못되었습니다.")
-            clearAll()
+        try {
+            const response = await fetch(`${server}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ userid: id, password })
+            })
+
+            if (response.status === 404) {
+                setLoginCheckLabel('존재하지 않는 아이디입니다.')
+            } else if (response.status === 401) {
+                setLoginCheckLabel('비밀번호가 일치하지 않습니다.')
+            } else if (response.status === 200) {
+                navigate("/")
+                const { userid } = await response.json()
+                alert(`${userid}님, 반갑습니다.`)
+                await login({userid})
+                clearAll()
+            }
+        } catch (e) {
+            console.error('[로그인] 서버 요청 중 오류가 발생했습니다', e)
         }
     }
 
